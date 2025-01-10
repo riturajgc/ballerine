9deda07e07b3a3581b709bb2d1b03a771c8d362d
Rituraj <rituraj@MacBookPro.lan>
2025-01-10 14:28:55 +0530

    feat: changes navbar menus

--- apps/backoffice-v2/src/common/components/organisms/Header/hooks/useNavbarLogic/useNavbarLogic.tsx
+++ apps/backoffice-v2/src/common/components/organisms/Header/hooks/useNavbarLogic/useNavbarLogic.tsx
@@ -1,7 +1,7 @@
 import { useFiltersQuery } from '@/domains/filters/hooks/queries/useFiltersQuery/useFiltersQuery';
 import { useFilterId } from '@/common/hooks/useFilterId/useFilterId';
 import { useCallback, useMemo } from 'react';
-import { Building, Goal, Home, Users } from 'lucide-react';
+import { Building, Goal, Home, Users, Ticket } from 'lucide-react';
 import { TRoutes, TRouteWithChildren } from '@/Router/types';
 import { useLocation } from 'react-router-dom';
 import { useCustomerQuery } from '@/domains/customer/hook/queries/useCustomerQuery/useCustomerQuery';
@@ -28,58 +28,53 @@ export const useNavbarLogic = () => {
       href: `/${locale}/home`,
       key: 'nav-item-Home',
     },
+    // {
+    //   text: 'Businesses',
+    //   icon: <Building size={20} />,
+    //   children: [
+    //     ...(customer?.config?.isMerchantMonitoringEnabled
+    //       ? [
+    //           {
+    //             text: 'Merchant Monitoring',
+    //             href: `/en/merchant-monitoring`,
+    //             key: 'nav-item-merchant-monitoring',
+    //           },
+    //         ]
+    //       : []),
+    //     ...(businessesFilters?.map(({ id, name }) => ({
+    //       filterId: id,
+    //       text: name,
+    //       href: `/${locale}/case-management/entities?filterId=${id}`,
+    //       key: `nav-item-${id}`,
+    //     })) ?? []),
+    //   ],
+    //   key: 'nav-item-businesses',
+    // },
     {
-      text: 'Businesses',
-      icon: <Building size={20} />,
+      text: 'Tickets',
+      icon: <Ticket size={20} />,
       children: [
-        ...(customer?.config?.isMerchantMonitoringEnabled
-          ? [
-              {
-                text: 'Merchant Monitoring',
-                href: `/en/merchant-monitoring`,
-                key: 'nav-item-merchant-monitoring',
-              },
-            ]
-          : []),
-        ...(businessesFilters?.map(({ id, name }) => ({
-          filterId: id,
-          text: name,
-          href: `/${locale}/case-management/entities?filterId=${id}`,
-          key: `nav-item-${id}`,
-        })) ?? []),
-      ],
-      key: 'nav-item-businesses',
-    },
-    {
-      text: 'Individuals',
-      icon: <Users size={20} />,
-      children: [
-        // {
-        //   text: 'Profiles',
-        //   href: `/en/profiles/individuals`,
-        //   key: 'nav-item-profile-individuals',
-        // },
         ...(individualsFilters?.map(({ id, name }) => ({
-          filterId: id,
-          text: name,
-          href: `/${locale}/case-management/entities?filterId=${id}`,
-          key: `nav-item-${id}`,
-        })) ?? []),
+            filterId: id,
+            text: name,
+            href: `/${locale}/case-management/entities?filterId=${id}`,
+            key: `nav-item-${id}`,
+          })) ?? []),
       ],
       key: 'nav-item-individuals',
-    },
-    {
-      text: 'Transaction Monitoring',
-      icon: <Goal size={20} />,
-      children: [
-        {
-          text: 'Alerts',
-          href: `/${locale}/transaction-monitoring/alerts`,
-          key: 'nav-item-alerts',
-        },
-      ],
-      key: 'nav-item-transaction-monitoring',
-    },
+    },    
+    // {
+    //   text: 'Transaction Monitoring',
+    //   icon: <Goal size={20} />,
+    //   children: [
+    //     {
+    //       text: 'Alerts',
+    //       href: `/${locale}/transaction-monitoring/alerts`,
+    //       key: 'nav-item-alerts',
+    //     },
+    //   ],
+    //   key: 'nav-item-transaction-monitoring',
+    // },
   ] satisfies TRoutes;
   const { pathname } = useLocation();
   const checkIsActiveFilterGroup = useCallback(
